80b801eee3b7f65098688fc8f55c9163a277f279
banawayz8387 <banawayz@gmail.com>
2025-06-20 20:54:33 +0300

    Add files via upload

---
+++ Trackable.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+// utms/transport/interfaces/Trackable.java
+package utms.interfaces;
+
+public interface Trackable {
+    void track();
+}
