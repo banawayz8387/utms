af2d98c6056c4089fb9883f65bb84cfa97d7fae4
banawayz8387 <banawayz@gmail.com>
2025-06-20 20:51:48 +0300

    Add files via upload

---
+++ UTMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.vu.utms;
+
+/**
+ *
+ * @author Administrator
+ */
+public class UTMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
